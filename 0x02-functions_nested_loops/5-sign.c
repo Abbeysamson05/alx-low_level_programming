180eb59b8624349f1d335e068fca5ae22a4fe327
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-12 13:39:20 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - check for numbers greater than 1, equal to 0 and less than 1.
+ * *@n: Return
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	putchar('-');
+	return (-1);
+}
