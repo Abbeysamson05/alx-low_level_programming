55857da27a4f8d4ebd02b3141ece03c8e17322b2
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-12 13:19:08 -0700

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower  - check if lowercase.
+ * @c: function parameter
+ * Return: 1 or 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
