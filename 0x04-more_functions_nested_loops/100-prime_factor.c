e68ccb80b439a4582e984b7c0892d0a58d60cb39
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-15 11:55:05 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * main  - Checks for prime factors
+ * Return: Void.
+ */
+int main(void)
+{
+	long j = 612852475143;
+	int largest;
+
+	while (j > 0)
+	{
+		if ((j % 2) != 0)
+		{
+			j = j / 2;
+			largest = 2;
+		}
+		else if ((j % 3) != 0)
+		{
+			j = j / 3;
+			largest = 3;
+		}
+		else if ((j % 17) != 0)
+		{
+			j = j / 17;
+			largest = 17;
+		}
+		else if ((j % 19) != 0)
+		{
+			j = j / 19;
+			largest = 19;
+		}
+		else if ((j % 31) != 0)
+		{
+			j = j / 31;
+			largest = 31;
+		}
+	}
+	return (largest);
+}
