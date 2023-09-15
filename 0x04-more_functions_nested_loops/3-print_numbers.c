9a971e09033de63f18827308c3b70d66e5976c60
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-14 21:57:32 -0700

    num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+task 3
+#include "main.h"
+/**
+ * print_numbers - Print digit 0 to 9
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
