419d5f3bdae16e411370508eb5ddab32ba1ce67b
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-14 23:16:41 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - Draw a diagonal line
+ * @n: function parameter
+ * Return: Void.
+ */
+void print_diagonal(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
