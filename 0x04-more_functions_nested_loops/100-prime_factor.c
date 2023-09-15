1c6847eb814853420c6b96a0919b186ddb16d978
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-15 12:48:11 -0700

    K

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main  - Checks for prime factors and print highest of the factors
@@ -10,31 +11,32 @@ int main(void)
 
 	while (j > 0)
 	{
-		if ((j % 2) != 0)
+		if ((j % 2) == 0)
 		{
 			j = j / 2;
 			largest = 2;
 		}
-		else if ((j % 3) != 0)
+		else if ((j % 3) == 0)
 		{
 			j = j / 3;
 			largest = 3;
 		}
-		else if ((j % 17) != 0)
+		else if ((j % 17) == 0)
 		{
 			j = j / 17;
 			largest = 17;
 		}
-		else if ((j % 19) != 0)
+		else if ((j % 19) == 0)
 		{
 			j = j / 19;
 			largest = 19;
 		}
-		else if ((j % 31) != 0)
+		else if ((j % 31) == 0)
 		{
 			j = j / 31;
 			largest = 31;
 		}
 	}
+	printf("%d", largest);
 	return (largest);
 }
