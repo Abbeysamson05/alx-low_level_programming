84fc5a71b27543825ac507456379184106c528aa
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-18 15:24:39 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * main - check the code
+ * swap_int - Swap the values of two variables
+ * @a: parameter
+ * @b: parameter
+ * Return: Void.
+ */
+void void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
