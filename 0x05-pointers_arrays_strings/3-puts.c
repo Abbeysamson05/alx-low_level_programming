dec422a7f4bd18ca3c95a1aa707e8a1f15be8912
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-18 16:18:49 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * _puts - Resets variable to 98 .
+ * @str: parameter
+ * Return: Void.
+ */
+void _puts(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		_putchar(str[length]);
+	}
+	_putchar('\n');
+}
