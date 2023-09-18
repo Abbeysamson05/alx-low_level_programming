31265769ecdcd1a0bb2004883aee4b718eba74cb
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-18 16:49:44 -0700

    Rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ * rev_string - Resets variable to 98 .
+ * @s: parameter
+ * Return: Void.
+ */
+void rev_string(char *s)
+{
+	int i;
+	char str[strlen(s)];
+
+	for (i = (strlen(i) - 1); i >= 0; i--)
+	{
+		str += i;
+	}
+}
