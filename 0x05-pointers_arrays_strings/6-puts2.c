936d33c990c0cd985cd1aad5e0d5d2a25625d035
Abbeysamson05 <ajayiabiodunsamson05@gmail.com>
2023-09-18 17:14:07 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ * puts2 - Resets variable to 98 .
+ * @str: parameter
+ * Return: Void.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+		_putchar('\n');
+	}
+}
